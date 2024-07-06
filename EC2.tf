fe7d2f3c36bf94f6b4f537954f5110e6cf1176b0
xsam951 <tadrissi.samuel@gmail.com>
2024-07-06 12:46:13 +0200

    creation of ec2

---
+++ EC2.tf
@@ -0,0 +1,25 @@
+# Get latest AMI ID of Amazon Linux 2
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Create EC2
+resource "aws_instance" "website-ec2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = var.ec2_instance_type
+  availability_zone = var.availability_zone
+  key_name = var.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.website_sg.id]
+  subnet_id = aws_subnet.public_subnet.id
+
+  tags = merge(local.tags, {
+    "Name" = "${var.tagName}-EC2"
+  }) 
+}
