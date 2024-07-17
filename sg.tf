0afe03c132585140fb9c9c762727c3f487c2a008
xsam951 <tadrissi.samuel@gmail.com>
2024-07-17 15:01:21 +0200

    implemented more security: added extra security group for elb, allowing http ingress only; adjusted ec2 scurity group to get http traffic only from the elb security group

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "website_sg" {
-  name        = "website_sg"
+  name        = "website-sg"
   description = "Security group for Website instance"
 
   vpc_id = aws_vpc.website_vpc.id
@@ -10,7 +10,36 @@ resource "aws_security_group" "website_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = var.cidr_block
+    
   }
+
+  ingress {
+    description = "HTTP from ELB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    #cidr_blocks = var.cidr_block
+    security_groups = [aws_security_group.elb_sg.id]
+  }
+
+  egress {
+    description = "All traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.cidr_block
+  }
+
+  tags = merge(local.tags, {
+    "Name" = "${var.tagName}-EC2-SG"
+  })    
+}
+
+resource "aws_security_group" "elb_sg" {
+  name        = "elb-sg"
+  description = "Security group for ELB"
+
+  vpc_id = aws_vpc.website_vpc.id
 
   ingress {
     description = "HTTP"
@@ -29,6 +58,6 @@ resource "aws_security_group" "website_sg" {
   }
 
   tags = merge(local.tags, {
-    "Name" = "${var.tagName}-SG"
+    "Name" = "${var.tagName}-ELB-SG"
   })    
 }
